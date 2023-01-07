4f6fc2323db6132decd05d35f651ec36dffa1bb3
Houssam Righi <hrighi01@gmail.com>
2023-01-06 22:35:20 -0500

    IEEE*

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -5,7 +5,7 @@ import { Register } from '../../components';
 const Header = () => {
   return (
     <div className='header_container'>
-      <h3 className='header_surtitle'>Ieee Concordia invites you to</h3>
+      <h3 className='header_surtitle'>IEEE Concordia invites you to</h3>
       <h1 className='header_title'>WARHACKS</h1>
       <h3 className='header_surtitle'>On Feburary 11th, 2023</h3>
       <h3 className='header_subtitle'>AKA where you learn how to build robots</h3>
