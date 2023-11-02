42ff745031bb253bf8a9ac1b9d2119b8738bb917
Webmaster-ieeeConcordia <114526522+Webmaster-ieeeConcordia@users.noreply.github.com>
2023-11-02 15:45:55 -0400

    Updated for 2024

--- src/containers/cta/CTA.jsx
+++ src/containers/cta/CTA.jsx
@@ -1,47 +1,61 @@
-import React from 'react'
-import './cta.css'
-import { Register } from '../../components'
+import React from "react";
+import "./cta.css";
+import { Register } from "../../components";
 
 const CTA = () => {
   return (
-    <div className='cta_container'>
+    <div className="cta_container">
+      <div className="cta_main">
+        <h2 className="cta_main_title">Hurry up!</h2>
+        <p className="cta_main_text">Places are limited</p>
+        <Register
+          text="Register Now!"
+          background="#63BAFF"
+          margin="10px"
+          href="https://forms.gle/ZTPC5EAKg3omDEK6A"
+        />
+      </div>
 
-        <div className='cta_main'>
-            <h2 className='cta_main_title'>Hurry up!</h2>
-            <p className='cta_main_text'>Places are limited</p>
-            <Register 
-                text='Register Now!'
-                background='#63BAFF'
-                margin='10px'
-                href= "https://docs.google.com/forms/d/e/1FAIpQLScV9RSNqekgSI8L-rZKJTPsq0vgwg_nYEdyXl-eUcARq4l6oA/viewform?usp=sf_link"
-            />
-        </div>
+      <div
+        style={{
+          width: "50%",
+          height: "0",
+          border: "1px solid rgba(240, 240, 241, 0.7)",
+        }}
+      ></div>
 
-        <div style={{width: "50%", height: "0", border: "1px solid rgba(240, 240, 241, 0.7)"}}></div>
+      <div className="cta_secondary">
+        <p className="cta_main_text">
+          Challenges will be done in teams of 3 or 4. You can invite your
+          friends or we can team you up.
+        </p>
+        <h2 className="cta_secondary_call">Invite your friends</h2>
+      </div>
 
-        <div className='cta_secondary'>
-            <p className='cta_main_text'>Challenges will be done in teams of 3 or 4.
-            You can invite your friends or we can team you up.</p>
-            <h2 className='cta_secondary_call' >Invite your friends</h2>
-        </div>
+      <div
+        style={{
+          width: "50%",
+          height: "0",
+          border: "1px solid rgba(240, 240, 241, 0.7)",
+        }}
+      ></div>
 
-        <div style={{width: "50%", height: "0", border: "1px solid rgba(240, 240, 241, 0.7)"}}></div>
-
-        <div className='cta_secondary'>
-            <p className='cta_main_text'>You can also check out previous Warhacks below!</p>
-            <Register 
-                text='Previous Warhacks'
-                background='#C2CCD6'
-                color="#000000"
-                boxShadow="none"
-                href="https://www.facebook.com/media/set/?set=a.2157921424286586&type=3"
-                margin="10px"
-            />
-            {/* <a className='cta_secondary_call' href='https://www.facebook.com/media/set/?set=a.2157921424286586&type=3'>Previous Warhacks</a> */}
-        </div>
-        
+      <div className="cta_secondary">
+        <p className="cta_main_text">
+          You can also check out previous Warhacks below!
+        </p>
+        <Register
+          text="Previous Warhacks"
+          background="#C2CCD6"
+          color="#000000"
+          boxShadow="none"
+          href="https://www.facebook.com/media/set/?set=a.637496271715785&type=3"
+          margin="10px"
+        />
+        {/* <a className='cta_secondary_call' href='https://www.facebook.com/media/set/?set=a.2157921424286586&type=3'>Previous Warhacks</a> */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import './header.css';
-import { Register } from '../../components';
+import React from "react";
+import "./header.css";
+import { Register } from "../../components";
 
 const Header = () => {
   return (
-    <div className='header_container'>
-      <h3 className='header_surtitle'>IEEE Concordia invites you to</h3>
-      <h1 className='header_title'>WARHACKS</h1>
-      <h3 className='header_surtitle'>On Feburary 11th, 2023</h3>
-      <h3 className='header_subtitle'>Learn how to build robots!</h3>
-      <Register 
-        text='Register Now!'
-        background='#63BAFF'
-        margin= '50px 0'
-        href= "https://docs.google.com/forms/d/e/1FAIpQLScV9RSNqekgSI8L-rZKJTPsq0vgwg_nYEdyXl-eUcARq4l6oA/viewform?usp=sf_link"
+    <div className="header_container">
+      <h3 className="header_surtitle">IEEE Concordia invites you to</h3>
+      <h1 className="header_title">WARHACKS</h1>
+      <h3 className="header_surtitle">On Feburary 18th, 2024</h3>
+      <h3 className="header_subtitle">Learn how to build robots!</h3>
+      <Register
+        text="Register Now!"
+        background="#63BAFF"
+        margin="50px 0"
+        href="https://forms.gle/ZTPC5EAKg3omDEK6A"
       />
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
