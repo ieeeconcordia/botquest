ed62e694a99674fcea97ab987cc7d141a0eff8c3
Houssam Righi <hrighi01@gmail.com>
2024-01-18 22:02:42 -0500

    Refactor WhatWarhacks component and update comments

--- src/containers/WhatWarhacks/Whatwarhacks.jsx
+++ src/containers/WhatWarhacks/Whatwarhacks.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import './whatwarhacks.css';
+import React from "react";
+import "./whatwarhacks.css";
 
 const WhatWarhacks = () => {
   return (
-    <div className='wiw_container'>
-      <h1 className='wiw_card_detail_title'>
-        About Warhacks
-      </h1>
-      <div style={{border: "1px solid #8D7365", width: '230px', height:'0px'}}></div>
-      <p className='wiw_card_detail_text'>
-      Warhacks is an introductory hardware Hackathon designed specifically for CEGEP students. 
-      Come spend the day with us, and you will get to build your first robot from scratch.
+    <div className="wiw_container">
+      <h1 className="wiw_card_detail_title">About Warhacks</h1>
+      <div
+        style={{ border: "1px solid #8D7365", width: "230px", height: "0px" }}
+      ></div>
+      <p className="wiw_card_detail_text">
+        Warhacks is an introductory hardware Hackathon designed specifically for
+        CEGEP students and First-year undergrads. Come spend the day with us,
+        and you will get to build your first robot from scratch.
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default WhatWarhacks
+export default WhatWarhacks;
