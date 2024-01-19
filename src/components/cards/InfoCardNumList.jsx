b7086aa2f2aefc108abc5293e9847255695222bf
Houssam Righi <hrighi01@gmail.com>
2024-01-18 21:44:39 -0500

    Update event date in InfoCardNumList component

--- src/components/cards/InfoCardNumList.jsx
+++ src/components/cards/InfoCardNumList.jsx
@@ -1,33 +1,34 @@
-import React from 'react'
-import './infocard.css'
+import React from "react";
+import "./infocard.css";
 
 const InfoCardNumList = (props) => {
   return (
     <div className="infocard_container">
-
       <div className="infocard_image">
-          <img
-            loading="lazy"
-            src= {props.imagesrc}
-            alt= "CardS"
-            className="image"
-            style={{verticalAlign: "bottom"}}
-          />
+        <img
+          loading="lazy"
+          src={props.imagesrc}
+          alt="CardS"
+          className="image"
+          style={{ verticalAlign: "bottom" }}
+        />
       </div>
 
       <div className="infocard_details">
-          <div className="infocard_title">It's as easy as 1-2-3</div>
-          <div className="infocard_list">
-            <ol>
-              <li>Fill the registration form to reserve your place.</li>
-              <li>Share with friends if you want to.</li>
-              <li>Come to Concordia on February 11th, ready to learn and have a good time.</li>
-            </ol>
-          </div>
+        <div className="infocard_title">It's as easy as 1-2-3</div>
+        <div className="infocard_list">
+          <ol>
+            <li>Fill the registration form to reserve your place.</li>
+            <li>Share with friends if you want to.</li>
+            <li>
+              Come to Concordia on January 28th, ready to learn and have a good
+              time.
+            </li>
+          </ol>
+        </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default InfoCardNumList
+export default InfoCardNumList;
