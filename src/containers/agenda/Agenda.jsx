72cbe461e310ee1d9ffb8b08f5dd1b3ecdc140e3
Houssam Righi <hrighi01@gmail.com>
2024-01-26 15:36:32 -0500

    Updated the agenda

--- src/containers/agenda/Agenda.jsx
+++ src/containers/agenda/Agenda.jsx
@@ -1,34 +1,37 @@
-import React from 'react'
-import './agenda.css'
+import React from "react";
+import "./agenda.css";
 
 const Agenda = () => {
   return (
-    <div className='agenda_container'>
-        <h1 className='agenda_title'>
-            Agenda
-        </h1>
-        <div style={{border: "1px solid #8D7365", width: '230px', height:'0px'}}></div>
-        
-        <div className='agenda_text'>
-          9 AM: Registration <br/>
-          <div className='agenda_text_line'></div>
-          10 AM : Opening Ceremony <br/>
-          <div className='agenda_text_line'></div>
-          10:30 AM: Tutorial <br/>
-          <div className='agenda_text_line'></div>
-          12 PM: Lunch <br/>
-          <div className='agenda_text_line'></div>
-          1 PM: Hacking <br/>
-          <div className='agenda_text_line'></div>
-          5 PM: Dinner <br/>
-          <div className='agenda_text_line'></div>
-          6 PM : Competition <br/>
-          <div className='agenda_text_line'></div>
-          8:30 PM: Closing ceremony
-        </div>
+    <div className="agenda_container">
+      <h1 className="agenda_title">Agenda</h1>
+      <div
+        style={{ border: "1px solid #8D7365", width: "230px", height: "0px" }}
+      ></div>
 
+      <div className="agenda_text">
+        9 AM: Registration + Breakfast <br />
+        <div className="agenda_text_line"></div>
+        9:30 AM: Opening Ceremony <br />
+        <div className="agenda_text_line"></div>
+        10 AM: Tutorial <br />
+        <div className="agenda_text_line"></div>
+        12:15 PM: Lunch <br />
+        <div className="agenda_text_line"></div>
+        12:45 PM: Explaining the rules <br />
+        <div className="agenda_text_line"></div>
+        1 PM: Hacking <br />
+        <div className="agenda_text_line"></div>
+        3 PM: Snack break <br />
+        <div className="agenda_text_line"></div>
+        5:30 PM: Dinner <br />
+        <div className="agenda_text_line"></div>
+        6:30 PM: Competition <br />
+        <div className="agenda_text_line"></div>
+        8:30 PM: Closing ceremony
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Agenda
+export default Agenda;
